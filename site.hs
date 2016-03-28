2ee9c16a5fac5bdebef48257bc75ab23ac14734a
Amar Shah <amar47shah@gmail.com>
2016-03-27 21:55:26 -0400

    Add rule for pages

--- site.hs
+++ site.hs
@@ -21,6 +21,12 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
+    match "pages/*" $ do
+        route idRoute
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
